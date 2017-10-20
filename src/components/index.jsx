a9d66f4ee2b577ccd850145f54ece91149f9e825
Pawel <pawel.html@gmail.com>
2017-10-20 11:32:32 +0200

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+
+    render(){
+        return(
+            <div>Hello World 3</div>
+        );
+    };
+
+};
+
+export default App;
+
+
